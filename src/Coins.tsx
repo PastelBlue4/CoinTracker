88f13a29c306c600be4e5edea374c0270a48e066
Romuru <pastelblue0721@gmail.com>
2022-03-19 22:51:58 +0900

    create ui add color type

--- src/Coins.tsx
+++ src/Coins.tsx
@@ -1,15 +1,39 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
-const Container = styled.div``;
+const Container = styled.div`
+  padding: 0px 20px;
+`;
 
-const Header = styled.header``;
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const CoinsList = styled.ul``;
 
-const Coin = styled.li``;
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  border-radius: 15px;
+  margin-bottom: 10px;
+  a {
+    padding: 20px;
+    transition: color 0.15s ease-in;
+    display: block;
+  }
+  &:hover {
+    a {
+      color: ${(props) => props.theme.accentColor};
+    }
+  }
+`;
 
 const CoinTitle = styled.h1`
-  color: ${(props) => props.theme.accentColor};
+  font-size: 48px;
+  color: ${(props) => props.theme.titleColor};
 `;
 
 const coins = [
@@ -50,7 +74,9 @@ function Coins() {
       </Header>
       <CoinsList>
         {coins.map((coin) => (
-          <Coin key={coin.id}>{coin.name}</Coin>
+          <Coin key={coin.id}>
+            <Link to={`${coin.id}`}>{coin.name} &rarr; </Link>
+          </Coin>
         ))}
       </CoinsList>
     </Container>
--- src/theme.ts
+++ src/theme.ts
@@ -3,5 +3,6 @@ import { DefaultTheme } from "styled-components";
 export const theme: DefaultTheme = {
   bgColor: "#4b4b4b",
   textColor: "#18dcff",
-  accentColor: "#32ff7e",
+  accentColor: "#18dcff",
+  titleColor: "#5352ed",
 };
