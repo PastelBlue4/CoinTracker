4056e00682e47e6d21f22a8180d40e724256fbc7
Romuru <pastelblue0721@gmail.com>
2022-11-10 23:58:27 +0900

    [feat] ui update

--- src/Coins.tsx
+++ src/Coins.tsx
@@ -1,16 +1,11 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
-import { useQuery } from "react-query";
-import { fetchCoins } from "./api";
 
 const Container = styled.div`
   padding: 0px 20px;
-  max-width: 480px;
-  margin: 0 auto;
 `;
 
 const Header = styled.header`
-  height: 10vh;
+  height: 15vh;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -24,71 +19,21 @@ const Coin = styled.li`
   border-radius: 15px;
   margin-bottom: 10px;
   a {
-    display: flex;
     padding: 20px;
-    transition: color 0.15s ease-in;
-    align-items: center;
+    transition: color 0.2s ease-in;
+    display: block;
   }
   &:hover {
     a {
       color: ${(props) => props.theme.accentColor};
     }
   }
+  margintop
+  
 `;
-
-const CoinTitle = styled.h1`
-  font-size: 48px;
-  color: ${(props) => props.theme.titleColor};
-`;
-
-const Loading = styled.span`
-  font-size: 30px;
-  text-align: center;
-  display: block;
-`;
-
-const CoinLogo = styled.img`
-  width: 30px;
-  height: 30px;
-  margin-right: 15px;
-`;
-
-interface ICoin {
-  id: string;
-  name: string;
-  symbol: string;
-  rank: number;
-  is_new: boolean;
-  is_active: boolean;
-  type: string;
-}
 
 function Coins() {
-  const { isLoading, data } = useQuery<ICoin[]>("allCoins", fetchCoins);
-
-  return (
-    <Container>
-      <Header>
-        <CoinTitle>코인</CoinTitle>
-      </Header>
-      {isLoading ? (
-        <Loading>"코인을 불러오는중..."</Loading>
-      ) : (
-        <CoinsList>
-          {data?.slice(0, 100).map((coin) => (
-            <Coin key={coin.id}>
-              <Link to={`/${coin.id}`} state={coin.name}>
-                <CoinLogo
-                  src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
-                />
-                {coin.name} &rarr;{" "}
-              </Link>
-            </Coin>
-          ))}
-        </CoinsList>
-      )}
-    </Container>
-  );
+  return <div> Coins</div>;
 }
 
 export default Coins;
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,16 +1,5 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Coin from "./Coin";
-import Coins from "./Coins";
-
 const Router = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/:coinId" element={<Coin />} />
-        <Route path="/" element={<Coins />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <div>Router</div>;
 };
 
 export default Router;
